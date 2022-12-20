dc684d3156d57e55b99f2572b30f9b0a571a0412
Andrei Oltean <olteanandreicj@gmail.com>
2022-12-20 22:32:24 +0200

    react typescript fundamentals 2

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 
 import GuestList from "./state/GuestList";
 import UserSearch from "./state/UserSearch";
+import {UserSearch as UserSearchRef}  from "./refs/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
     return (
         <div>
             <UserSearch />
             <hr />
+            <UserSearchRef />
+            <hr />
             <GuestList />
+            <hr />
+            <EventComponent />
         </div>
     )
 }
